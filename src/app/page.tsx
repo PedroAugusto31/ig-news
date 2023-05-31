9923f3313f4f6f80d3dbf16cd142e513debec4e1
Pedro Augusto <pedroaugustoa47@gmail.com>
2023-05-31 17:21:23 -0300

    feat/chore: Header component created and icons added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import styles from "../styles/Home.module.scss";
-
 export default function Home() {
 	return (
 		<>
-			<h1 className={styles.title}>Hello, world!!</h1>
+			<h1>Hello, world!!</h1>
 			<p>s2</p>
 		</>
 	);

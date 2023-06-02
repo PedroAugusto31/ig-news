a270bfc2a24c42d42ba04a3ee2561b148010d2f1
Pedro Augusto <pedroaugustoa47@gmail.com>
2023-06-02 17:01:31 -0300

    feat: signIn and subscribe button made and home page completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import Image from "next/image";
+import styles from "./home.module.scss";
+import { SubscribeButton } from "../components/SubscribeButton";
+
 export default function Home() {
 	return (
-		<>
-			<h1>Hello, world!!</h1>
-			<p>s2</p>
-		</>
+		<main className={styles.contentContainer}>
+			<section className={styles.hero}>
+				<span>Hey, welcome</span>
+				<h1>
+					News about the <span>React</span> world.
+				</h1>
+				<p>
+					Get access to all the publications <br />
+					<span>for $9.90 month</span>
+				</p>
+				<SubscribeButton />
+			</section>
+			<Image
+				src="/images/avatar.svg"
+				alt="Girl coding"
+				width={336}
+				height={521}
+			/>
+		</main>
 	);
 }
